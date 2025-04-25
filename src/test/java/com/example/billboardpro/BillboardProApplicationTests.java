f9a804da89264dace3163e8e87baa7bd48c47869
nhungtong <tongthihongnhung9999@gmail.com>
2025-04-25 15:10:00 +0700

    create project

---
+++ src/test/java/com/example/billboardpro/BillboardProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.billboardpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillboardProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
