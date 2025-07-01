20bfb65f069664cd9a206cbda72e4fa971035a37
dannhanong <thannong0512@gmail.com>
2025-07-01 14:03:23 +0700

    2

--- src/main/java/com/techbytedev/signboardmanager/config/SecurityConfig.java
+++ src/main/java/com/techbytedev/signboardmanager/config/SecurityConfig.java
@@ -103,7 +103,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         // configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://127.0.0.1:3000"));
-        configuration.setAllowedOrigins(Arrays.asList("https://ai2.vn", "http://localhost:3000"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
